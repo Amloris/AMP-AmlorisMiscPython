117f4f40acbdabb8dc2c469268ae321ebf9eb36f
Amloris <aaronrob@ksu.edu>
2018-11-09 13:16:06 -0600

    Added CLI prompts and window naming for GetFile() and GetDir()

--- FileIO.py
+++ FileIO.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python
 
 """
-fileIO.py
+AMP-AmlorisMiscPython: FileIO.py
 -------------------------------------------------------------------------------
 Aaron Robertson
 General Use
@@ -13,11 +13,12 @@ File Description:
 
 Changelog:
 11/08/18 - Modified from the original fileIO script.
+11/09/18 - Removed specialized file loading functions. Added filtering to GetFile()
+11/09/18 - Added CLI prompts and window naming for GetFile() and GetDir()
 -------------------------------------------------------------------------------
 """
 
 #Libraries
-#import numpy as np                                #Math
 import os                                          #Input/Output
 import sys                                         #Input/Output
 if sys.version_info[0] < 3:
@@ -32,83 +33,91 @@ else:
 '''File Systems'''
 '''-------------------------------------------------------------------------'''
 
-def GetDir(initial_dir="./", default_dir="./", quiet = False):
+def GetDir(initial_dir="./", default_dir="./", cli_prompt="Select a directory:", 
+           window_name='Select Directory', quiet = False):    
     '''Opens a window that allows a user to select a directory.
        Returns the directory path.
-    '''
-    
-    '''Opens a window that allows a user to select a directory, or provides
-       one if none is specified.
        
        Inputs
        -------
-       initial_dir : str
+       initial_dir : str (optional)
            The starting directory of the search. Selects the default
            directory of the script if no input is given.
-       default_dir : str
+       default_dir : str (optional)
            The absolute path to the default directory to return if no user 
            input is given.
-       quiet : bool
+       cli_prompt: str (optional)
+           The command line interface prompt for directory selection.
+       window_name : str (optional)
+           Name of the GUI window.
+       quiet : bool (optional)
            If true, the logging will be surpressed.
        
        Outputs
        -------
        dname : str
-           The name of the selected directory.
+           The path of the selected directory.
     '''    
     
     #Set Window Attributes
     InitFileWindow()
     
     #Get Specified Directory
-    if not quiet: print("Select a directory:")
+    if not quiet: print(cli_prompt)
     if IsPython3:
          dir_name = str((filedialog.askdirectory(initialdir=initial_dir,      \
-                        title='Select Directory')) or default_dir)
+                        title=window_name)) or default_dir)
     else:
          dir_name = str((tkFileDialog.askdirectory(initialdir=initial_dir,    \
-                        title='Select Directory')) or default_dir) 
+                        title=window_name)) or default_dir) 
     dir_name = os.path.normpath(dir_name)
     if not quiet: print('%s\n' %dir_name)
 
     return dir_name
 
-def GetFile(initial_dir="", default_file="", quiet = False):
-    '''Opens a window that allows a user to select a specific file or selects
-       a default file if no user input is given. A file extension filter can
-       be applied to narrow results.
+def GetFile(initial_dir="", default_file="", filters=[("CSV","*.csv"), ("Text","*.txt")],
+            cli_prompt="Select a file:", window_name='Select File', quiet = False):
+    '''Opens a window that allows a user to select a file.
+       Filters can be applied to narrow results.
+       Returns the file path.
        
        Inputs
        -------
-       initial_dir : str
+       initial_dir : str (optional)
            The starting directory for the file search. Defaults to the
            directory of the script if no input is given.
-       default_file : str
-           The path to the default file to return if no user input is given.
-       filter  : dict
-       
-       quiet : bool
+       default_file : str (optional)
+           The path to the default file to return if no user input is given. This
+           function will fail if a user does not select a file and no default file
+           is given.
+       filter  : list of tuples (optional)
+           A file filter composed of strings in a dict. Valid input is show below
+               [("CSV","*.csv"), ("Text","*.txt")]
+           If no argument is specified only .txt and .csv files will be shown.
+       cli_prompt: str (optional)
+           The command line interface prompt for file selection.
+       window_name : str (optional)
+           Name of the GUI window.
+       quiet : bool (optional)
            If true, the logging will be surpressed.
        
        Outputs
        -------
        fname : str
-           The name of the selected file.
+           The path of the selected file.
     '''
     
     #Set Window Attributes
     InitFileWindow()
     
     #Get Specified File
-    if not quiet: print("Select a file:")
+    if not quiet: print(cli_prompt)
     if IsPython3:
-         fname = str((filedialog.askopenfilename(initialdir=initial_dir,      \
-                      title = 'Select File', filetypes=[("CSV","*.csv"),      \
-                      ("Text","*.txt")])) or default_file)
+         fname = str((filedialog.askopenfilename(initialdir=initial_dir,          \
+                      title = window_name, filetypes=filters)) or default_file)
     else:
-         fname = str((tkFileDialog.askopenfilename(initialdir=initial_dir,    \
-                      title = 'Select File', filetypes=[("CSV","*.csv"),      \
-                      ("Text","*.txt")])) or default_file)
+         fname = str((tkFileDialog.askopenfilename(initialdir=initial_dir,        \
+                      title = window_name, filetypes=filters)) or default_file)
          
     fname = os.path.normpath(fname)
     if not quiet: print("%s\n" %fname)
@@ -144,140 +153,6 @@ def InitFileWindow():
     ###########################################################################    
         
     return root
-    
-
-'''File Loading/Saving'''
-'''-------------------------------------------------------------------------'''
-
-def LoadData(fname):
-    '''
-    Loads the results files for the indent profiler. The data is separated into
-    a header, a global data block, and data blocks for the individual indents.
-    
-    Inputs
-    ------
-    fname : str
-        The path to the file. The extension must be .txt or .csv to be loaded    
-    
-    Outputs
-    -------
-    header : class
-        Header block data will be stored in a class for easy access.
-    global : ndarray
-        Global data is loaded into a [10xm] array. m = number of included indents.
-        Formated in the form of,
-            [Row]
-            [IndentNumber]
-            [Volume]
-            [Pitch]
-            [Width]
-            [SideWallAreaL]
-            [SideWallAreaR]
-            [AvgDepth]
-            [AvgSideAngleL]
-            [AvgSideAngleR]
-    indents :
-        
-    Notes
-    -----
-    A file extension check is provided in this function as a default. This 
-    function will be used in multiple projects where file extensions are not
-    guaranteed to be verfied prior to the LoadData() function call.
-    '''
-
-    #Set Extension
-    filename, file_ext = os.path.splitext(fname)
-    if (file_ext == '.txt'):
-        delim = None
-    elif (file_ext == '.csv'):
-        delim = ','
-    else:
-        sys.exit("TERMINATE:LOAD_DATA:INVALID_EXTENSION")
-
-    #Load Header
-    header_length = 19
-    header_index = np.arange(0, header_length)        #Header data rows
-    with open(fname,'r') as fin:
-        for i, line in enumerate(fin):
-            if i in header_index:
-                line_val = line.strip().split(delim)[-1]
-                if i == 0: WireProfile      = str(line_val) 
-                if i == 1: TotalSampleLength= float(line_val)    
-                if i == 2: PtsPerRev        = int(line_val) 
-                if i == 3: XSmoothing       = int(line_val)
-                if i == 4: XMedian          = int(line_val)
-                if i == 5: YAveraging       = int(line_val)
-                if i == 6: YMedian          = int(line_val)
-                if i == 7: DataState        = str(line_val)
-                if i == 8: TimeStamp        = str(line_val)
-                if i == 9: Rotation         = float(line_val)
-                if i == 10: Twist           = float(line_val)
-                if i == 11: TotalIndents    = int(line_val)
-                if i == 12: XPtInterval     = float(line_val)
-                if i == 13: YPtInterval     = float(line_val)
-                if i == 14: AvgRadius       = float(line_val)      
-                if i == 15: ExcludedIndents = str(line_val)
-                if i == 16: 
-                    Orientations = line.strip('\n').split(delim)[1:]
-                    Orientations = [float(i) for i in Orientations]
-                if i == 17:
-                    AvgWidthPerRow = line.strip('\n').split(delim)[1:]
-                    AvgWidthPerRow = [float(i) for i in AvgWidthPerRow]
-                if i ==18:
-                    IndentDistance = line.strip('\n').strip(';').split(delim)[1:]
-                    IndentDistance = [float(i) for i in IndentDistance]
-                
-    header = HeaderInfo(WireProfile, TotalSampleLength, PtsPerRev, XSmoothing,   \
-                        XMedian, YAveraging, YMedian, DataState, TimeStamp,      \
-                        Rotation, Twist, TotalIndents, XPtInterval, YPtInterval, \
-                        AvgRadius, ExcludedIndents, Orientations, AvgWidthPerRow,\
-                        IndentDistance)
-
-    #Load Global Data 
-    header_length = 19
-    header_index = np.arange(header_length, header_length+10)        #Header data rows
-    global_block = np.array((0,0))
-    with open(fname,'r') as fin:
-        for i, line in enumerate(fin):
-            if i in header_index:
-                line_val = line.strip('\n').split(delim)[1:]
-                line_val = [float(i) for i in line_val]
-                line_val = np.asarray(line_val)
-                if (i == header_length):
-                    global_block = line_val
-                else:
-                    global_block = np.vstack((global_block,line_val))
-
-    return header, global_block
-
-
-class HeaderInfo():
-    '''Used to store the header data of results files.'''
-    
-    def __init__(self, WireProfile, TotalSampleLength, PtsPerRev, XSmoothing,
-				 XMedian, YAveraging, YMedian, DataState, TimeStamp, Rotation,
-                 Twist, TotalIndents, XPtInterval, YPtInterval, AvgRadius,
-                 ExcludedIndents, Orientations, AvgWidthPerRow, IndentDistance):
-        self.WireProfile = WireProfile
-        self.TotalSampleLength = TotalSampleLength
-        self.PtsPerRev = PtsPerRev
-        self.XSmoothing = XSmoothing
-        self.XMedian = XMedian
-        self.YAveraging = YAveraging
-        self.YMedian = YMedian
-        self.DataState = DataState
-        self.TimeStamp = TimeStamp
-        self.Rotation = Rotation    
-        self.Twist = Twist
-        self.TotalIndents = TotalIndents
-        self.TimeStamp = TimeStamp
-        self.XPtInterval = XPtInterval
-        self.YPtInterval = YPtInterval
-        self.AvgRadius = AvgRadius
-        self.ExcludedIndents = ExcludedIndents
-        self.Orientations = Orientations
-        self.AvgWidthPerRow = AvgWidthPerRow
-        self.IndentDistance = IndentDistance
 
 
 if __name__ == "__main__":
@@ -285,8 +160,5 @@ if __name__ == "__main__":
        Running this command will execute the test suite.
     '''   
 
-
     file_temp = GetFile()
     dir_temp = GetDir()
-    
-    #root.mainloop()
